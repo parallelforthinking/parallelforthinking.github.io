78a60a45143cf8ddd0cc211d8aa931608652b10a
sdlovecraft <sdh@eml.cc>
2016-09-01 01:19:01 +1000

    add closing of windows

---
+++ _includes/close-toggle.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $(".close-button").click(function(event) {
+    var id = event.target.id;
+
+    if (id == "archive-close") {
+      $("#archive-panel").hide();
+      $('#archive').prop('checked', false);
+    }
+    else if (id== "contributors-close"){
+      $("#contributors-panel").hide();
+      $('#contributors').prop('checked', false);
+    }
+    else if (id == "information-close") {
+      $("#information-panel").hide();
+      $('#information').prop('checked', false);
+    }
+    else if (id == "social-media-close") {
+      $("#social-media-panel").hide();
+      $('#social-media').prop('checked', false);
+    }
+  });
+});
